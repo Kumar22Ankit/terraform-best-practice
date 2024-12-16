7d0cbdbcfb662b74b499c88b4a072b2652c140e5
Ankit Kumar <146810887+Kumar22Ankit@users.noreply.github.com>
2024-12-16 18:09:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example0" {
+resource "aws_instance" "example" {
   ami           = "ami-0e2c8caa4b6378d8c" 
   instance_type = "t2.micro"
 
