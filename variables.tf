befadc07a6b1b14b72a88bc160c99bdc38897de2
Ankit Kumar <146810887+Kumar22Ankit@users.noreply.github.com>
2024-12-16 16:05:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  description = "AWS region to deploy resources"
-  default     = "us-east-1"
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"  
 }
-
