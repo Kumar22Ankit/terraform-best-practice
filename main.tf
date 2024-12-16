07e978443881c41448f12519fe2cf343232ae085
Ankit Kumar <146810887+Kumar22Ankit@users.noreply.github.com>
2024-12-16 16:04:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
-provider "aws" {
-  region = "us-east-1" 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  
+      region  = "us-east-1"
+    }
+  }
 }
+
 
 resource "aws_instance" "example" {
   ami           = "ami-0e2c8caa4b6378d8c" 
